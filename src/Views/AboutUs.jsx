84cdb5123a116107c0f7ebae61804c9e371a8b25
Rohit Sharma <90538552+RohitWeb21@users.noreply.github.com>
2022-02-08 07:46:37 +0530

    about

--- src/Views/AboutUs.jsx
+++ src/Views/AboutUs.jsx
@@ -22,17 +22,57 @@ const AboutUs = () => {
               marketing initiatives and communications, and may manage a
               portfolio of products.
               <br />
+              <br />
               With the utmost professionalism, BS Group provide high-quality
               designs and perfect services. For the convenience of our clients,
               we have an active support system through which you can contact us.
-              <br />The main vision of the BS Group is "Coming together is a
+              <br />
+              <br />
+              The main vision of the BS Group is "Coming together is a
               beginning, keeping together is progress, and working together is a
               success."
+              <br/>
+              <br/>
+              <p id="demo" className="collapse">
+              BS Group intends to generate employment at a steady rate. We aim
+              to create a granular view of who needs help to keep their job and
+              to equip the youth with the right opportunities by generating a
+              productive workforce. BS Group started its operations with one
+              member and now we have more than 100+ members.
+              <br />
+              <br />
+           
+              The company is authorised as Registered LLP and as MSME under
+              Government of India. Moreover, it is also recognised as start-up
+              under Government of India.
+              <br />
+              <br />
+             
+              It is the first company to tie up with Zomato for its employees'
+              benefit. More than 2000+ designs have been created/flourished by
+              our company and we are Constantly expanding new talent, we are on
+              a hiring spree with rapid growth and entering into different
+              companies, such as Naya Gurukul, which is a trade mark of BS
+              GROUP.
+              <br />
+              <br />
+            
+              We have recently been featured in one of the global magazines-
+              Business Connect, within one year of getting into business. Our
+              success speaks through our satisfied client base.
             </p>
-
+            </p>
+            
+           
             {/* button    */}
             <div>
-              <button className="DiscoverMoreBtn">Discover More</button>
+              <button
+                className="DiscoverMoreBtn"
+                data-bs-toggle="collapse"
+                data-bs-target="#demo"
+              >
+                Discover More
+              </button>
             </div>
           </article>
         </section>
